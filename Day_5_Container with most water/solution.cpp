374b8ec11b2b277d3d27430466c3b4d83eff5002
Palak4244 <thakurpalaksingh466@gmail.com>
2026-06-15 22:19:49 +0530

    Add maxArea function using two-pointer technique
    Implemented an optimized two-pointer approach to calculate the maximum area of water that can be contained.

---
+++ Day_5_Container with most water/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) { // optimized approach , two pointer approach , O(logn)
+        int maxwater = 0;
+        int lp = 0, rp = height.size() - 1;
+        while(lp<rp){
+            int w = (rp-lp);
+            int ht = min(height[lp] , height[rp]);
+            int currwt = w*ht;
+            maxwater = max(currwt, maxwater);
+            height[lp]<height[rp] ? lp++ : rp-- ;
+        }
+        return maxwater;
+        
+    }
+};
