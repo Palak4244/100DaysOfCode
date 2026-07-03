67d44b310ed396a01ab4b84930eb6eb38e7f1906
Palak Singh <palak.24b01011083@abes.ac.in>
2026-07-03 22:51:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.43%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) return n;  // base cases
+        int prev1 = 1, prev2 = 2; // ways for step 1 and 2
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+};
