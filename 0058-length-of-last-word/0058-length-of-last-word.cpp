3972101506cad4adbbf75cb5d9e2a7fc77c2be78
Palak Singh <palak.24b01011083@abes.ac.in>
2026-07-17 21:26:14 +0530

    Time: 0 ms (100%), Space: 9 MB (35%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int i = n - 1;
+
+        // Step 1: skip trailing spaces
+        while (i >= 0 && s[i] == ' ') i--;
+
+        // Step 2: count last word length
+        int length = 0;
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
